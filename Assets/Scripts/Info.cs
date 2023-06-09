c38faf19ae8304dc3095b674568b9e5c1e0e853d
dMinsz <dsminzks@gmail.com>
2023-06-09 16:03:26 +0900

    feat: Make Infomation for this game in UI , and OnClick Event
    게임 빌드시 스터디용 게임 이라는걸 알려주기위해 몇까지 글내용을 UI형식으로 추가하였다.

---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info : MonoBehaviour
+{
+    public void SetStart() 
+    {
+        this.gameObject.SetActive(false);
+    }
+}
